6485c087c6453d42c4f8ef1ee1fdf2286ccbbc76
baccarisverige-ux <baccari.sverige@gmail.com>
2026-08-18 00:03:18 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+    return (
+        <div style={{ padding: "40px", fontFamily: "sans-serif", textAlign: "center" }}>
+              <h1 style={{ color: "#2D6A4F" }}>Movera Host</h1>
+                    <p>Bienvenue sur la nouvelle version du site.</p>
+                        </div>
+                          );
+                          }
+
+                          export default App;
+
+
+
+
