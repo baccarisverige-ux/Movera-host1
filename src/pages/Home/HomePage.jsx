3f520bbe513df07fc7827e1692fe181c238e8a29
baccarisverige-ux <baccari.sverige@gmail.com>
2026-08-18 18:09:02 +0200

    feat(home): use new Tout globe PNG

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -4,7 +4,7 @@ import '../../styles/home-b225.css'
 import '../../styles/home-b225-block2.css'
 import '../../styles/home-b225-block3.css'
 
-const ALL_CATEGORY_GLOBE = '/Movera-host1/assets/all-category-globe-v2.svg'
+const ALL_CATEGORY_GLOBE = '/Movera-host1/assets/all-category-globe-v3.png'
 const GUESTHOUSE_CATEGORY_ICON = '/Movera-host1/assets/maison-hote-button.png'
 
 function SearchIcon(){return <svg viewBox="0 0 24 24" aria-hidden="true"><circle cx="11" cy="11" r="7"/><path d="M21 21l-4.3-4.3"/></svg>}
