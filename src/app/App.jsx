6dac4a93ef37b696cfe0bb285413874a10d341c3
baccarisverige-ux <baccari.sverige@gmail.com>
2026-08-19 21:16:10 +0200

    refactor: use Home and Search public APIs

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,6 @@
-import { HomeAccessibility } from '../features/home/HomeAccessibility.jsx'
+import { HomeAccessibility } from '../features/home/index.js'
 import { ResilienceLayer } from '../features/resilience/ResilienceLayer.jsx'
-import { SearchTransitionHost } from '../features/search/SearchTransitionHost.jsx'
+import { SearchTransitionHost } from '../features/search/index.js'
 import { GlobalErrorBoundary } from './error-boundary/GlobalErrorBoundary.jsx'
 import { AppProviders } from './providers/AppProviders.jsx'
 import { AppRouter, navigate } from './router/index.jsx'
