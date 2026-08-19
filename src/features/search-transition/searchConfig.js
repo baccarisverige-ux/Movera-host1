55a9665a63f2f634b5577c397e351825334fd98a
baccarisverige-ux <baccari.sverige@gmail.com>
2026-08-19 20:41:09 +0200

    refactor: centralize Search transition configuration

---
+++ src/features/search-transition/searchConfig.js
@@ -0,0 +1,20 @@
+export const SEARCH_TRANSITION_TIMING = Object.freeze({
+  openMs: 980,
+  completeMs: 560,
+  readyMs: 820,
+  stepMs: 220,
+})
+
+export const SEARCH_RECENTS_KEY = 'movera-search-recents-v1'
+
+export const SEARCH_OVERVIEW_VIEWPORT = Object.freeze({
+  lat: 34.15,
+  lng: 9.55,
+  zoom: 7,
+})
+
+export const SEARCH_PANEL_LIMITS = Object.freeze({
+  maxHeight: 570,
+  minHeight: 455,
+  viewportRatio: 0.64,
+})
