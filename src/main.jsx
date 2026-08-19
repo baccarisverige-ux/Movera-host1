0c4512c24d362a9ccad4f2bc53c767f6fcc551de
baccarisverige-ux <baccari.sverige@gmail.com>
2026-08-19 02:32:30 +0200

    Link categories scroll timing to welcome section

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/partner-category.css'
+import './styles/home-scroll-link.css'
+import './homeScrollLink.js'
 import App from './app/App.jsx'
 
 createRoot(document.getElementById('root')).render(
