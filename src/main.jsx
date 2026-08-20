7604602d012f9a1a82a6e06436f4c2b6e5fa3adb
baccarisverige-ux <baccari.sverige@gmail.com>
2026-08-20 02:31:02 +0200

    feat: enable global Motion configuration

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app/App.jsx'
+import { MoveraMotionProvider } from './shared/motion/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MoveraMotionProvider>
+      <App />
+    </MoveraMotionProvider>
   </StrictMode>,
 )
