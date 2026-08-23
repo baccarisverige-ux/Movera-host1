58c58b474332cd7bf199cb894d38ecef2e76b43e
baccarisverige-ux <baccari.sverige@gmail.com>
2026-08-23 08:32:28 +0200

    Add Tozeur and Tabarka map destinations

--- src/features/map/constants/map.constants.js
+++ src/features/map/constants/map.constants.js
@@ -7,6 +7,8 @@ export const DESTINATION_VIEWPORTS = Object.freeze({
   tunis: { lat: 36.8065, lng: 10.1815, zoom: 12 },
   sousse: { lat: 35.8256, lng: 10.6369, zoom: 12 },
   djerba: { lat: 33.8076, lng: 10.8451, zoom: 11 },
+  tozeur: { lat: 33.9197, lng: 8.1335, zoom: 12 },
+  tabarka: { lat: 36.9544, lng: 8.758, zoom: 12 },
   nabeul: { lat: 36.4561, lng: 10.7376, zoom: 12 },
   bizerte: { lat: 37.2746, lng: 9.8739, zoom: 12 },
 })
