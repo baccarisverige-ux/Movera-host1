665d14d06674389d75f619e6e1ce1a73e9a5eae6
baccarisverige-ux <baccari.sverige@gmail.com>
2026-08-26 00:34:30 +0200

    Add map list item Motion config

--- src/features/map/motion/mapOfferSheetMotion.config.js
+++ src/features/map/motion/mapOfferSheetMotion.config.js
@@ -13,3 +13,17 @@ export const MAP_OFFER_SHEET_MOTION = Object.freeze({
   toggleThreshold: 0.72,
   velocityProjectionSeconds: 0.16,
 })
+
+export const MAP_OFFER_ITEM_MOTION = Object.freeze({
+  activeScale: 1,
+  enterScale: 0.985,
+  enterY: 12,
+  exitScale: 0.985,
+  exitY: -6,
+  inactiveScale: 0.992,
+  initialOpacity: 0.82,
+  layout: true,
+  stagger: 0.02,
+  tapScale: 0.988,
+  spring: Object.freeze({ stiffness: 410, damping: 35, mass: 0.74 }),
+})
